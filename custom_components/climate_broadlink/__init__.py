b85b32b3bfea620b8b90b87d72a798b2b1ed8cc7
Daniel Fernando Lourusso <dflourusso@gmail.com>
2026-01-19 11:52:00 -0300

    fix: update init file

--- custom_components/climate_broadlink/__init__.py
+++ custom_components/climate_broadlink/__init__.py
@@ -11,9 +11,9 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "climate")
-    )
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["climate"])
+
     return True
 
 
